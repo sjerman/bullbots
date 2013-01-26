03b0bf8a3f09d22a3db9744b47a95ca7b40f6daa
Admin <Admin@10.149.5.242>
2013-01-26 10:17:52 -0700

    made drive jaguar do something

--- Ultimate_Ascent/src/org/bullbots/ascend/hardware/DriveJaguar.java
+++ Ultimate_Ascent/src/org/bullbots/ascend/hardware/DriveJaguar.java
@@ -4,10 +4,37 @@
  */
 package org.bullbots.ascend.hardware;
 
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
 /**
  *
  * @author Admin
  */
 public class DriveJaguar {
+    CANJaguar jag;
     
+    int P = 1;
+    int I = 1;
+    int D = 1;
+    
+    public DriveJaguar(int number){
+        try{
+            jag = new CANJaguar(number, CANJaguar.ControlMode.kSpeed);
+            jag.setPID(P, I, D);
+            jag.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+        }
+        catch(CANTimeoutException ex){
+            ex.printStackTrace();
+        }
+    
+    }
+    
+    public void set(double value){
+        try {
+            jag.setX(value);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
